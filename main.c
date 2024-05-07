e7f51e6244e9771d3306879859d7705f59a2db9c
pionmich <129657706+pionmich@users.noreply.github.com>
2024-05-07 10:50:08 +0200

    Vorlage Aufgabe 3

--- main.c
+++ main.c
@@ -1,43 +1,36 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
+#include <stdio.h>
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/adc.h"
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
+#include "driverlib/fpu.h"
 
-// Praeprozessor-Makros
-#define BUFFER_SIZE 1000
+// Praepozessor-Makros
 #define SAMPLERATE 44000
-#define AVERAGE_MAX (VALUE_MAX - OFFSET) * (VALUE_MAX - OFFSET) //berechnet maximalen Average mit Hilfe eines Präprozessor-Makros
-#define AVERAGE_POT(threshold_value) (10^ ((threshold_value * AVERAGE_MAX)/8))   //Maximaler Grenzwert der Skala in Achteln. -> Exponentielle Skala
 
-// Funktionen-Deklarationen
+//Funktionen-Deklarationen
 void adcIntHandler(void);
 void setup(void);
-
-// Konstanten
-const uint8_t HIGH = 0xFF;    // LED an
-const uint8_t LOW = 0;        // LED aus
-const uint32_t OFFSET = 1400;   // Da bei keinem Audiosignal, der ADC trotzdem rund 1450 ausgibt.
-const uint32_t VALUE_MAX = 2600; // mit meinem Setup experimentell erfasster Maximalwert, da mein Audiointerface wenig Leistung hat. Sonst 4095 bei 3.3V
+// hier nach Bedarf noch weitere Funktionsdeklarationen einfuegen
 
 // globale Variablen
-// hier die benÃ¶tigten globalen Variablen, wie den Ringbuffer einfuegen
-uint32_t ringBuffer[BUFFER_SIZE];    //array mit n=BUFFER_SIZE Elementen
-uint32_t writeIndex = 0;
-uint32_t readIndex = 0;
-uint32_t average = 0;
+const float DoublePi = 6.283185308;
+int32_t bufferSample[440];
+// hier nach Bedarf noch weitere globale Variablen einfuegen
 
-void main(void){ // nicht veraendern!! Bitte Code in adcIntHandler einfuegen
+void main(void){ // nicht veraendern!! Bitte Code in adcIntHandler einfuegen 
     setup();
-
     while(1){}
 }
 
-void setup(void){// konfiguriert den MiKrocontroller
-    // konfiguriere System-Clock
+void setup(void){//konfiguriert den Mikrocontroller
+
+    // konfiguriere SystemClock
     SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_OSC_MAIN|SYSCTL_XTAL_16MHZ);
     uint32_t period = SysCtlClockGet()/SAMPLERATE;
 
@@ -46,6 +39,12 @@ void setup(void){// konfiguriert den MiKrocontroller
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+
+    // aktiviere Gleitkommazahlen-Modul
+    FPUEnable();
+    FPUStackingEnable();
+    FPULazyStackingEnable();
+    FPUFlushToZeroModeSet(FPU_FLUSH_TO_ZERO_EN);
 
     // konfiguriere GPIO
     GPIOPinTypeADC(GPIO_PORTE_BASE,GPIO_PIN_2);
@@ -68,79 +67,10 @@ void setup(void){// konfiguriert den MiKrocontroller
 
 }
 
-void adcIntHandler (void){
-    uint32_t adcInputValue;
-    ADCSequenceDataGet(ADC0_BASE,3,&adcInputValue);
-    uint32_t adcInputValueSqrtOffset = ((adcInputValue - OFFSET) * (adcInputValue - OFFSET))/ BUFFER_SIZE;   // Wert des ADC durch OFFSET anpassen und quadrieren
-    average = average + adcInputValueSqrtOffset - ringBuffer[readIndex];   // den Average durch eine RunningSum berechnen
-
-    readIndex = (readIndex + 1) % BUFFER_SIZE;  //readindex fortführen
-    ringBuffer[writeIndex] = adcInputValueSqrtOffset;  //neuen Wert in den Buffer Schreiben
-    writeIndex = (writeIndex + 1) % BUFFER_SIZE;    //writeindex fortführen
-
-
-    if ((average > 0 )&&(average < AVERAGE_POT(1)) ) //AVERAGE_POT(x) = AVERAGE_POT_MAX * (x/8)
-    {
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_0) , HIGH ) ;
-        GPIOPinWrite (GPIO_PORTB_BASE, (
-                GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 |
-                GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
-                GPIO_PIN_7) , LOW ) ;
-    }
-    else if ((average >= (AVERAGE_POT(1)) )&&(average < AVERAGE_POT(2)) ) //AVERAGE_POT(x) = AVERAGE_POT_MAX * (x/8)
-    {
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_0 |
-                GPIO_PIN_1) , HIGH ) ;
-        GPIOPinWrite (GPIO_PORTB_BASE, ( GPIO_PIN_2 | GPIO_PIN_3 |
-                GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
-                GPIO_PIN_7) , LOW ) ;
-    }
-    else if ((average >= (AVERAGE_POT(2)) )&&(average < AVERAGE_POT(3)) ) //AVERAGE_POT(x) = AVERAGE_POT_MAX * (x/8)
-    {
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_0 |
-                GPIO_PIN_1 | GPIO_PIN_2) , HIGH ) ;
-        GPIOPinWrite (GPIO_PORTB_BASE, ( GPIO_PIN_3 |
-                GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
-                GPIO_PIN_7) , LOW ) ;
-    }
-    else if ((average >= (AVERAGE_POT(3)) )&&(average < AVERAGE_POT(4)) ) //AVERAGE_POT(x) = AVERAGE_POT_MAX * (x/8)
-    {
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_0 |
-                GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3) , HIGH ) ;
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
-                GPIO_PIN_7) , LOW ) ;
-    }
-    else if ((average >= (AVERAGE_POT(4)) )&&(average < AVERAGE_POT(5)) ) //AVERAGE_POT(x) = AVERAGE_POT_MAX * (x/8)
-    {
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_0 |
-                GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 |
-                GPIO_PIN_4) , HIGH ) ;
-        GPIOPinWrite (GPIO_PORTB_BASE, ( GPIO_PIN_5 | GPIO_PIN_6 |
-                GPIO_PIN_7) , LOW ) ;
-    }
-    else if ((average >= (AVERAGE_POT(5)) )&&(average < AVERAGE_POT(6)) ) //AVERAGE_POT(x) = AVERAGE_POT_MAX * (x/8)
-    {
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_0 |
-                GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 |
-                GPIO_PIN_4 | GPIO_PIN_5) , HIGH ) ;
-        GPIOPinWrite (GPIO_PORTB_BASE, ( GPIO_PIN_6 |
-                GPIO_PIN_7) , LOW ) ;
-    }
-    else if ((average >= (AVERAGE_POT(6)) )&&(average < AVERAGE_POT(7)) ) //AVERAGE_POT(x) = AVERAGE_POT_MAX * (x/8)
-    {
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_0 |
-                GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 |
-                GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6) , HIGH ) ;
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_7) , LOW ) ;
-    }
-    else if ((average >= (AVERAGE_POT(7)) )&&(average <= AVERAGE_POT(8)) ) //AVERAGE_POT(x) = AVERAGE_POT_MAX * (x/8)
-    {
-        GPIOPinWrite (GPIO_PORTB_BASE, (GPIO_PIN_0 |
-                GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 |
-                GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
-                GPIO_PIN_7) , HIGH ) ;
-    }
-
-    // am Ende von adcIntHandler, Interrupt-Flag loeschen
-    ADCIntClear(ADC0_BASE,3);
+void adcIntHandler(void){
+   // Bitte Code hier einfuegen
+   // ...
+   
+   // am Ende von adcIntHandler, Interrupt-Flag loeschen
+   ADCIntClear(ADC0_BASE,3);
 }
